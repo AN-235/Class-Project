fef711ce13f9cfb043fe00b6ec328f5cc1cfb913
Daniel <danieljreyes2001@gmail.com>
2025-10-13 14:22:51 -0400

    created flask health api check

--- api/flaskAPI.py
+++ api/flaskAPI.py
@@ -1,2 +1,7 @@
 import flask
+import pandas as pd
 app = flask.Flask(__name__)
+
+@app.route('/api/health')
+def health():
+    return flask.jsonify({"status": "OK"})
--- client/streamlit_client.py
+++ client/streamlit_client.py
@@ -1,2 +1,6 @@
 import streamlit as st
 import pandas as pd
+
+
+df = pd.read_csv("../data/2022-nov16.csv")
+
